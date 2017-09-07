90cda939701191154b9e5912cac33bb280e4188b
Luzian Serafin <luzianserafin@gmail.com>
2017-09-07 20:12:32 +0200

    Additional tests. WIP

--- contracts/TicTacToeGame.sol
+++ contracts/TicTacToeGame.sol
@@ -3,6 +3,17 @@ pragma solidity ^0.4.4;
 contract TicTacToeGame {
 	
 	uint8[3][3] public board;
+	address public opponent;
+
+	function startGame() public returns (bool) {
+		if(opponent == address(0)) { //No Oponent yet
+			opponent = msg.sender;
+		}
+	}
+
+	function getOpponent() public returns (address) {
+		return opponent;
+	}
 
 	function placeOnBoard(uint xpos,uint ypos) public returns (uint) {
 		require(xpos >= 0 && xpos <= 3);
--- test/TicTacToeTest.js
+++ test/TicTacToeTest.js
@@ -8,4 +8,26 @@ contract('TicTacToeGame', function(accounts) {
       assert.equal(result.toNumber(), 0, "0 wasn't in this position");
     });
   });
+
+  it("should have no opponent after the deployment", function() {
+    return TicTacToeGame.deployed().then(function(instance) {
+      return instance.getOpponent.call();
+    }).then(function(result) {
+      assert.equal(result, 0, "0 wasn't in this position");
+    });
+  });
+
+  it("should have one opponent after the game start", function() {
+    var gameintstance;
+    return TicTacToeGame.deployed().then(function(instance) {
+      gameintstance = instance;
+      return gameintstance.startGame.call();
+    }).then(function(start) {
+      return gameintstance.getOpponent.call();
+    }).then(function(result) {
+      assert.equal(result, 0, "0 wasn't in this position");
+    });
+
+  });
+
 });
